010b3c0be881ca252d98cde0a6f9e3f4d76af97b
abd-ulbasit <basit01035@gmail.com>
2023-07-28 11:20:58 +0500

    enable drag for touch screen

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -14,38 +14,46 @@ const Draggable: React.FC<DraggableProps> = ({ children, initialPosition, resetP
     const [currentPosY, setCurrentPosY] = useState(initialPosition.y);
 
     useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
+        const handleMove = (e: MouseEvent | TouchEvent) => {
             if (isDragging) {
-                const deltaX = e.clientX - startPosX;
-                const deltaY = e.clientY - startPosY;
+                const clientX = 'touches' in e ? e.touches[0]!.clientX : e.clientX;
+                const clientY = 'touches' in e ? e.touches[0]!.clientY : e.clientY;
+                const deltaX = clientX - startPosX;
+                const deltaY = clientY - startPosY;
                 setCurrentPosX(currentPosX + deltaX);
                 setCurrentPosY(currentPosY + deltaY);
-                setStartPosX(e.clientX);
-                setStartPosY(e.clientY);
+                setStartPosX(clientX);
+                setStartPosY(clientY);
             }
         };
 
-        const handleMouseUp = () => {
+        const handleEnd = () => {
             setIsDragging(false);
             document.body.style.cursor = 'default';
         };
 
         if (isDragging) {
             document.body.style.cursor = 'grabbing';
-            document.addEventListener('mousemove', handleMouseMove);
-            document.addEventListener('mouseup', handleMouseUp);
+            document.addEventListener('mousemove', handleMove);
+            document.addEventListener('touchmove', handleMove);
+            document.addEventListener('mouseup', handleEnd);
+            document.addEventListener('touchend', handleEnd);
         }
 
         return () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
+            document.removeEventListener('mousemove', handleMove);
+            document.removeEventListener('touchmove', handleMove);
+            document.removeEventListener('mouseup', handleEnd);
+            document.removeEventListener('touchend', handleEnd);
         };
     }, [isDragging, currentPosX, currentPosY, startPosX, startPosY]);
 
-    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    const handleStart = (e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
         setIsDragging(true);
-        setStartPosX(e.clientX);
-        setStartPosY(e.clientY);
+        const clientX = 'touches' in e ? e.touches[0]!.clientX : e.clientX;
+        const clientY = 'touches' in e ? e.touches[0]!.clientY : e.clientY;
+        setStartPosX(clientX);
+        setStartPosY(clientY);
     };
 
     useEffect(() => {
@@ -57,7 +65,8 @@ const Draggable: React.FC<DraggableProps> = ({ children, initialPosition, resetP
     return (
         <div
             className="relative transition-transform duration-300"
-            onMouseDown={handleMouseDown}
+            onMouseDown={handleStart}
+            onTouchStart={handleStart}
             style={{ transform: `translate(${currentPosX}px, ${currentPosY}px)` }}
         >
             {children}
