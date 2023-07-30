a9bb9e5a1a0d318e58cc3f584e5a1296d86dca81
abd-ulbasit <basit01035@gmail.com>
2023-07-30 22:42:04 +0500

    add  ReactQuery DevTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,13 @@ import { ThemeProvider } from "~/components/theme-provider";
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
-
+const queryClient = new QueryClient();
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout,
   session: Session | null,
@@ -23,9 +25,13 @@ const MyApp: AppType<AppPropsWithLayout> = ({
   const getLayout: (page: ReactElement) => ReactNode = (Component as NextPageWithLayout).getLayout ?? ((page) => page);
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem >
-      <SessionProvider session={session}>
-        {getLayout(<Component {...pageProps} />)}
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+
+        <SessionProvider session={session}>
+          {getLayout(<Component {...pageProps} />)}
+        </SessionProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
